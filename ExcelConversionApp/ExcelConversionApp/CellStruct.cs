f10ede01e9dff72f59099b10f907414a84482977
Christian Kendall <ckendal3@msudenver.edu>
2019-01-11 12:43:33 -0700

    Add CellStruct
    ***
    Beginning of restructuring code to be more maintainable and readable. Will begin the phase of trying to make things more organized before additional features.
    ***

---
+++ ExcelConversionApp/ExcelConversionApp/CellStruct.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelConversionApp.Cell
+{
+    public struct CellStruct
+    {
+        /// <summary>
+        /// This is the cell type used for determing what type of information is stored
+        /// </summary>
+        ECellType cellType;
+
+        /// <summary>
+        /// The cell's string value
+        /// </summary>
+        string stringValue;
+
+        /// <summary>
+        /// The cell's numerical value
+        /// </summary>
+        double numericalValue;
+
+        /// <summary>
+        /// Initialize the Cell's String Value
+        /// </summary>
+        /// <param name="value"></param>
+        public CellStruct(string value)
+        {
+            cellType = ECellType.String;
+            stringValue = value;
+
+            numericalValue = -1;
+        }
+
+        /// <summary>
+        /// Initialize the Cell's Numerical Value
+        /// </summary>
+        /// <param name="value"></param>
+        public CellStruct(double value)
+        {
+            cellType = ECellType.Numerical;
+            numericalValue = value;
+
+            stringValue = "";
+        }
+
+    }
+
+    public enum ECellType
+    {
+        Numerical,
+        String,
+        Special
+    };
+}
